df04a3ffda38a04827f18f297273e7c7ad0282d2
Miroslav Holec <mirek.holec@hotmail.com>
2019-01-25 15:25:03 +0100

    Cleanup

--- src/Apic.Contracts/Infrastructure/Transfer/Result.cs
+++ src/Apic.Contracts/Infrastructure/Transfer/Result.cs
@@ -6,53 +6,5 @@ namespace Apic.Contracts.Infrastructure.Transfer
 	{
 		public T Data { get; set; }
         public List<string> Messages { get; set; }
-
-  //      public static DataResult<T> Ok(T data)
-		//{
-		//	return new DataResult<T>()
-		//	{
-		//		Data = data,
-		//		Code = ResultCodes.Ok
-		//	};
-		//}
-
-		//public new static DataResult<T> ClientBadRequest(string message)
-		//{
-		//	var result =  new DataResult<T>()
-		//	{
-		//		Code = ResultCodes.BadRequest
-		//	};
-
-		//	result.AddMessage(message);
-
-		//	return result;
-		//}
-
-		//public new static DataResult<T> Error(string message)
-		//{
-		//	var result = new DataResult<T>()
-		//	{
-		//		Code = ResultCodes.Error
-		//	};
-
-		//	result.AddMessage(message);
-
-		//	return result;
-		//}
-
-		//public static DataResult<T> NotFound(string message = null)
-		//{
-		//	var result = new DataResult<T>()
-		//	{
-		//		Code = ResultCodes.NotFound
-		//	};
-
-		//	if (!string.IsNullOrEmpty(message))
-		//	{
-		//		result.AddMessage(message);
-		//	}
-
-		//	return result;
-		//}
 	}
 }
